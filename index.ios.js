e78f342f24950c71d3092ed9bcfc17174e4cecda
iamtylerd <tylerdanielphotography@gmail.com>
2016-11-15 12:56:36 -0600

    version 1 complete

--- index.ios.js
+++ index.ios.js
@@ -42,13 +42,31 @@ class GroceryApp extends Component {
         rowHasChanged: (row1, row2) => row1 !== row2,
       })
     };
+    this.itemsRef = firebaseApp.database().ref();
   }
 
   componentDidMount() {
+    this.listenForItems(this.itemsRef);
+  }
+
+  listenForItems(itemsRef) {
+    itemsRef.on('value', (snap) => {
+
+      // get children as an array
+      var items = [];
+      snap.forEach((child) => {
+        items.push({
+          title: child.val().title,
+          _key: child.key
+        });
+      });
+
       this.setState({
-        dataSource: this.state.dataSource.cloneWithRows([{ title: 'Pizza' }])
-      })
-    }
+        dataSource: this.state.dataSource.cloneWithRows(items)
+      });
+
+    });
+  }
 
   render() {
     return (
@@ -58,16 +76,45 @@ class GroceryApp extends Component {
 
         <ListView dataSource={this.state.dataSource} renderRow={this._renderItem.bind(this)} style={styles.listview} />
 
-        <ActionButton title="Add" />
+        <ActionButton title="Add" onPress={this._addItem.bind(this)}></ActionButton>
 
       </View>
     );
   }
 
+  _addItem() {
+    AlertIOS.prompt(
+      'Add New Item',
+      null,
+      [
+        {
+          text: 'Add',
+          onPress: (text) => {
+            this.itemsRef.push({ title: text })
+          }
+        },
+      ],
+      'plain-text'
+    );
+  }
+
   _renderItem(item) {
-      return (
-        <ListItem item={item}  />
+
+    const onPress = () => {
+      AlertIOS.prompt(
+        'Complete',
+        null,
+        [
+          {text: 'Complete', onPress: (text) => this.itemsRef.child(item._key).remove()},
+          {text: 'Cancel', onPress: (text) => console.log('Cancel')}
+        ],
+        'default'
       );
+    };
+
+    return (
+      <ListItem item={item} onPress={onPress} />
+    );
   }
 };
 
